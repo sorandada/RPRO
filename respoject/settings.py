949ae4bee708210aa2e73ca68f901326e7a26d5c
sky <sora.nkz.ddd@gmail.com>
2023-04-19 01:16:17 +0900

    UpDate 2023/4/19

--- respoject/settings.py
+++ respoject/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'あなたのSECRET_KEYを入力'
-SECRET_KEY = 'django-insecure-0mxj&r7x#f@4@d-#6od+gr(vf_vt#nk@tysyolo$)edw^ihi@l'
-
+SECRET_KEY = 'あなたのSECRET_KEYを入力'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
